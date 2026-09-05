0f341f775460bc0c8ea86a75e9f380bc466eaef9
k.kotov <bazed85@gmail.com>
2026-09-05 12:34:30 +0100

    Update 202609005123000_create_estimates_deliveries.sql

--- supabase/migrations/202609005123000_create_estimates_deliveries.sql
+++ supabase/migrations/202609005123000_create_estimates_deliveries.sql
@@ -1,2 +1,3 @@
 CREATE TABLE estimates (
-  id bigint CONSTRAINT estimates_pk
+  id bigint CONSTRAINT estimates_pk PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  site_id bigint NOT NULL CONSTRAINT estimates_site_id_fk 
