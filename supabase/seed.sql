94d0e759044729ed963377ff323cacfbdbd144f0
k.kotov <bazed85@gmail.com>
2026-09-05 10:58:13 +0100

    Update seed.sql

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,1 +1,8 @@
+INSERT INTO units (code, name, sort_order) VALUES 
+('m3', 'cubic meter', 1),
+('kg', 'kilogram', 2),
+('pcs', 'pieces', 3);
 
+INSERT INTO materials (article_no, name, unit_id) VALUES
+('CEM II/B-L', 'CEMENT', 2),
+('', 'PHONIQUE plasterboard board 2500x1200x13 mm', 3);
