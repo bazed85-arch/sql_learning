72b0b453d38b6f7d3658d1da406490899dae8ae4
k.kotov <bazed85@gmail.com>
2026-09-11 12:50:15 +0100

    Update 20261109124600_estimate_revisions.sql

--- supabase/migrations/20261109124600_estimate_revisions.sql
+++ supabase/migrations/20261109124600_estimate_revisions.sql
@@ -1,1 +1,3 @@
-
+ALTER TABLE estimates ADD COLUMN revision int NOT NULL DEFAULT 1, 
+  DROP CONSTRAINT estimates_number_uq, 
+  ADD CONSTRAINT estimates_number_revision_uq UNIQUE (number, revision);
